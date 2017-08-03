5274f1d394b06c4565c682f09af9698da8640882
cclauss <cclauss@bluewin.ch>
2017-08-03 07:56:39 +0200

    from __future__ import print_function

--- snowball/clemency/ida-plugin/clemency-symbols.py
+++ snowball/clemency/ida-plugin/clemency-symbols.py
@@ -2,9 +2,11 @@
 Saves all function names in idb to symbol map for org's Clemency debugger.
 """
 
+from __future__ import print_function
 import os
 import idaapi
 from idaapi import *
+
 
 def get_symbol_map():
     """
@@ -19,11 +21,12 @@ def get_symbol_map():
     output_lines = []
     for i, (ea, (name, size)) in enumerate(sorted(functions.items())):
         if len(name) > 255:
-            print "ClemSym: truncating name", name
+            print("ClemSym: truncating name", name)
         name = name[:255]
         line = "%d: %s @ %07x %d" % (i, name, ea, size)
         output_lines.append(line)
     return '\n'.join(output_lines)
+
 
 def save_symbols():
     """
@@ -32,7 +35,7 @@ def save_symbols():
     input_file_path = idaapi.get_input_file_path()
 
     if not os.path.exists(input_file_path):
-        print "ClemSym: warning: {} does not exist.".format(input_file_path)
+        print("ClemSym: warning: {} does not exist.".format(input_file_path))
 
     output_path = input_file_path + '.map'
 
@@ -42,7 +45,7 @@ def save_symbols():
         with open(output_path, 'rb') as orig_fd:
             orig_data = orig_fd.read()
         if orig_data == new_data:
-            print "ClemSym: symbol map on disk is already up to date"
+            print("ClemSym: symbol map on disk is already up to date")
             return
 
         # Always backup as we *really* don't want to kill someone's
@@ -52,8 +55,9 @@ def save_symbols():
             bak_ctr += 1
         os.rename(output_path, output_path + '.bak' + str(bak_ctr))
 
-    print "ClemSym: writing symbols to", output_path
+    print("ClemSym: writing symbols to", output_path)
     with open(output_path, 'wb') as output_fd:
         output_fd.write(new_data)
+
 
 save_symbols()
