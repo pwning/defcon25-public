8c23b48d606859c546e198f52f69791a94db1bbe
cclauss <cclauss@bluewin.ch>
2017-08-03 07:23:13 +0200

    from __future__ import print_function for Python 3

--- clemency-as/assembler.py
+++ clemency-as/assembler.py
@@ -1,9 +1,11 @@
-import subprocess
-import parser
-import instrs
-import os
+#!/usr/bin/env python
 
+from __future__ import print_function
+import os
+import parser
+import subprocess
 import tempfile
+
 
 def parse_args(argv):
     import argparse
@@ -12,20 +14,22 @@ def parse_args(argv):
     parser.add_argument('outfile', nargs='?', help='Output file')
     return parser.parse_args(argv)
 
+
 def main(argv):
     args = parse_args(argv)
 
     if args.outfile is None:
         args.outfile = os.path.splitext(args.infile)[0] + '.bin'
 
-    print "Assembling %s to %s..." % (args.infile, args.outfile)
+    print("Assembling %s to %s..." % (args.infile, args.outfile))
 
     asm = subprocess.check_output(['cpp', '-xc++', args.infile])
 
     out = parser.Assembler().assemble(asm)
-    print "Output: %d nytes" % (len(out)/9)
+    print("Output: %d nytes" % (len(out) / 9))
     with open(args.outfile, 'wb') as outf:
         outf.write(out.force_str())
+
 
 def assemble_string(s):
     t = tempfile.NamedTemporaryFile(delete=False)
@@ -35,6 +39,7 @@ def assemble_string(s):
     os.unlink(t.name)
     out = parser.Assembler().assemble(asm)
     return out
+
 
 if __name__ == '__main__':
     import sys
