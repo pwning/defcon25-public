54a041f774d0664f21559563f73515f8782ab433
cclauss <cclauss@bluewin.ch>
2017-08-03 07:46:37 +0200

    print() function and xrange() --> range()
    xrange() was renamed to range() in Python 3
    
    Also used `with open() as` to ensure file pointers get a close()

--- snowball/clemency/test.py
+++ snowball/clemency/test.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
+from __future__ import print_function
 import ctypes
 import pyclemency
 import struct
 
-buf = open('hello.u16', 'rb').read()
+with open('hello.u16', 'rb') as in_file:
+    buf = in_file.read()
 pc = 0x90d
 while True:
     size = 6
@@ -12,10 +14,10 @@ while True:
     if size == 0:
         break
     code = (ctypes.c_uint16 * size)()
-    for x in xrange(size):
-        code[x] = struct.unpack('<H', buf[(pc+x)*2:(pc+x+1)*2])[0]
+    for x in range(size):
+        code[x] = struct.unpack('<H', buf[(pc + x) * 2:(pc + x + 1) * 2])[0]
     inst = pyclemency.disassemble(pc, code)
-    print '%08X\t%s' % (pc, inst.str)
+    print('%08X\t%s' % (pc, inst.str))
     if inst.id == 0:
         break
-    pc += inst.size
+pc += inst.size
